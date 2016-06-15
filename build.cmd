85594307c7eca00c8d8ccfbb093a7ce77a209aa9
Axel Heer <axel@heer.eu>
2016-06-15 21:17:57 +0200

    Enable AppVeyor

--- build.cmd
+++ build.cmd
@@ -4,6 +4,7 @@ dotnet restore
 
 dotnet build src\*\project.json --configuration Debug
 dotnet build src\*\project.json --configuration Release
-
 dotnet build test\*\project.json --configuration Debug
 dotnet build test\*\project.json --configuration Release
+
+dotnet pack src\NeinMath --configuration Release
--- pack.cmd
+++
@@ -1,5 +0,0 @@
-@echo off
-
-dotnet restore
-
-dotnet pack src\NeinMath --configuration Release
