38fc9e296d7b3df044267efadb103163ec49e47f
Axel Heer <axel@heer.eu>
2016-06-16 20:53:39 +0200

    Improve CI build

--- test.cmd
+++ test.cmd
@@ -1,13 +1,10 @@
 @echo off
 
-set test=%~dp0test
-set results=%~dp0TestResults
-
 set opencover=%UserProfile%\.nuget\packages\OpenCover\4.6.519\tools\OpenCover.Console.exe
 set reportgenerator=%UserProfile%\.nuget\packages\ReportGenerator\2.4.5\tools\ReportGenerator.exe
 
-if not exist %results% mkdir %results%
+if not exist TestResults mkdir TestResults
 
-"%opencover%" -target:dotnet.exe -targetargs:"test \"%test%\NeinMath.Tests\"" -output:"%results%\NeinMath.xml" -register:user -filter:+[NeinMath]*
+"%opencover%" -target:dotnet.exe -targetargs:"test test\NeinMath.Tests --configuration Release -xml TestResults\NeinMath.result.xml" -output:TestResults\NeinMath.report.xml -register:user -filter:+[NeinMath]*
 
-"%reportgenerator%" -reports:"%results%\NeinMath.xml" -targetdir:"%results%\report"
+"%reportgenerator%" -reports:TestResults\NeinMath.report.xml -targetdir:TestResults\report
