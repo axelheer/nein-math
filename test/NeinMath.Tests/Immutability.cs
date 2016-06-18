428a978a852e26c7574f252c6035daadfd0a2f90
Axel Heer <axel@heer.eu>
2016-06-18 19:47:07 +0200

    Update Immutability

--- test/NeinMath.Tests/Immutability.cs
+++ test/NeinMath.Tests/Immutability.cs
@@ -3,57 +3,60 @@ using Xunit;
 
 namespace NeinMath.Tests
 {
-    public sealed class Immutability : IDisposable
+    public static class Immutability
     {
-        readonly Integer left;
-        readonly Integer right;
-        readonly Integer other;
-
-        readonly string leftValue;
-        readonly string rightValue;
-        readonly string otherValue;
-
-        Immutability(Integer value)
-            : this(value, default(Integer))
+        public static IDisposable Guard<T>(T v0)
         {
+            return new ImmutabilityGuard<T>(v0);
         }
 
-        Immutability(Integer left, Integer right)
-            : this(left, right, default(Integer))
+        public static IDisposable Guard<T>(T v0, T v1)
         {
+            return new ImmutabilityGuard<T>(v0, v1);
         }
 
-        Immutability(Integer left, Integer right, Integer other)
+        public static IDisposable Guard<T>(T v0, T v1, T v2)
         {
-            leftValue = left.ToString();
-            rightValue = right.ToString();
-            otherValue = other.ToString();
-
-            this.left = left;
-            this.right = right;
-            this.other = other;
+            return new ImmutabilityGuard<T>(v0, v1, v2);
         }
 
-        public static IDisposable Guard(Integer value)
+        sealed class ImmutabilityGuard<T> : IDisposable
         {
-            return new Immutability(value);
-        }
+            readonly T v0;
+            readonly T v1;
+            readonly T v2;
 
-        public static IDisposable Guard(Integer left, Integer right)
-        {
-            return new Immutability(left, right);
-        }
+            readonly string t0;
+            readonly string t1;
+            readonly string t2;
 
-        public static IDisposable Guard(Integer left, Integer right, Integer other)
-        {
-            return new Immutability(left, right, other);
-        }
+            public ImmutabilityGuard(T v0)
+                : this(v0, default(T))
+            {
+            }
 
-        public void Dispose()
-        {
-            Assert.Equal(leftValue, left.ToString());
-            Assert.Equal(rightValue, right.ToString());
-            Assert.Equal(otherValue, other.ToString());
+            public ImmutabilityGuard(T v0, T v1)
+                : this(v0, v1, default(T))
+            {
+            }
+
+            public ImmutabilityGuard(T v0, T v1, T v2)
+            {
+                t0 = v0?.ToString();
+                t1 = v1?.ToString();
+                t2 = v2?.ToString();
+
+                this.v0 = v0;
+                this.v1 = v1;
+                this.v2 = v2;
+            }
+
+            public void Dispose()
+            {
+                Assert.Equal(t0, v0?.ToString());
+                Assert.Equal(t1, v1?.ToString());
+                Assert.Equal(t2, v2?.ToString());
+            }
         }
     }
 }
