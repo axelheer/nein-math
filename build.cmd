e49c613fcbe9b5116050f990e7a97ff674336ef0
Axel Heer <axel@heer.eu>
2016-12-16 18:55:05 +0100

    Clean-up visual studio stuff

--- build.cmd
+++ build.cmd
@@ -6,7 +6,7 @@ dotnet restore || goto :eof
 
 if not defined build_options call init.cmd || goto :eof
 
-dotnet build **\*\project.json %build_options:Release=Debug% || goto :eof
-dotnet build **\*\project.json %build_options% || goto :eof
+dotnet build **\project.json %build_options:Release=Debug% || goto :eof
+dotnet build **\project.json %build_options% || goto :eof
 
 dotnet pack src\NeinMath %build_options% || goto :eof
