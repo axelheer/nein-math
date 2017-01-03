d4f1ef6990a584a8a236abff2a90b7df37a06589
Axel Heer <axel@heer.eu>
2017-01-03 15:13:35 +0100

    Fix code coverage

--- test.cmd
+++ test.cmd
@@ -1,11 +1,11 @@
 @echo off
 
 set opencover=%UserProfile%\.nuget\packages\OpenCover\4.6.519\tools\OpenCover.Console.exe
-set reportgenerator=%UserProfile%\.nuget\packages\ReportGenerator\2.5.1\tools\ReportGenerator.exe
+set reportgenerator=%UserProfile%\.nuget\packages\ReportGenerator\2.5.2\tools\ReportGenerator.exe
 
 if not exist TestResults mkdir TestResults || goto :eof
 
-"%opencover%" -target:dotnet.exe -targetargs:"test test\NeinMath.Tests --configuration Release --framework net461 -xml TestResults\NeinMath.netframework.result.xml" -output:TestResults\NeinMath.netframework.report.xml -register:user -filter:+[NeinMath]* -returntargetcode || goto :eof
-"%opencover%" -target:dotnet.exe -targetargs:"test test\NeinMath.Tests --configuration Release --framework netcoreapp1.0 -xml TestResults\NeinMath.netcoreapp.result.xml" -output:TestResults\NeinMath.netcoreapp.report.xml -register:user -filter:+[NeinMath]* -returntargetcode || goto :eof
+"%opencover%" -target:dotnet.exe -targetargs:"test test\NeinMath.Tests --configuration Release --framework net461 -xml TestResults\NeinMath.netframework.result.xml" -output:TestResults\NeinMath.netframework.report.xml -register:user -filter:+[NeinMath]* -returntargetcode -oldstyle || goto :eof
+"%opencover%" -target:dotnet.exe -targetargs:"test test\NeinMath.Tests --configuration Release --framework netcoreapp1.1 -xml TestResults\NeinMath.netcoreapp.result.xml" -output:TestResults\NeinMath.netcoreapp.report.xml -register:user -filter:+[NeinMath]* -returntargetcode -oldstyle || goto :eof
 
 "%reportgenerator%" -reports:TestResults\*.report.xml -targetdir:TestResults\report -reporttypes:Badges;Html || goto :eof
